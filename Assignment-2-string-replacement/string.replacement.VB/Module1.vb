b20e65b51e3d93151578a2c8de2c79683b90c20f
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:04:14 +0500

    Second string manipulation assignment: string-replacement

---
+++ Assignment-2-string-replacement/string.replacement.VB/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str As String
+        Dim result As String
+        Dim chr As String
+        Dim repl As String
+        Dim i As Integer
+
+        str = ""
+        result = ""
+        chr = ""
+        repl = ""
+        i = 0
+
+        Console.Write("Enter your string: ")
+        str = Console.ReadLine()
+        Console.Write("Character to be replaced: ")
+        chr = Console.ReadLine()
+        Console.Write("Replacement: ")
+        repl = Console.ReadLine()
+
+        For i = 1 To Len(str)
+            If chr = Mid(str, i, 1) Then
+                result = result & repl
+            Else
+                result = result & Mid(str, i, 1)
+            End If
+        Next
+
+        Console.WriteLine("Result: " & result)
+        Console.ReadKey()
+
+    End Sub
+
+
+
+End Module
