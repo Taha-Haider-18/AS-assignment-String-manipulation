8210bec2edfad94238f8bc8dd883af69a7859144
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:24:13 +0500

    Seventh string manipulation assignment: both ends

---
+++ Assignment-7-both-ends/both.ends.VB/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim s As String
+
+        s = ""
+
+        Console.Write("String: ")
+        s = Console.ReadLine()
+
+        If Len(s) < 2 Then
+            Console.WriteLine("S output: " & s)
+        Else
+            Console.WriteLine("S output: " & Left(s, 2) & Right(s, 2))
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
+
