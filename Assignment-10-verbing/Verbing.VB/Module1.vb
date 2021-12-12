bba0f430a7feb219ded208b47069ca0e44b7e4d3
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:30:34 +0500

    Tenth string manipulation assignment: verbing

---
+++ Assignment-10-verbing/Verbing.VB/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+
+        Dim str As String
+
+        str = ""
+
+        Console.Write("String: ")
+        str = Console.ReadLine()
+
+        If Len(str) > 2 Then
+            If "ing" = Right(str, 3) Then
+                Console.WriteLine("Result: " & str & "ly")
+            Else
+                Console.WriteLine("Result: " & str & "ing")
+            End If
+        Else
+            Console.WriteLine("Result: " & str)
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
