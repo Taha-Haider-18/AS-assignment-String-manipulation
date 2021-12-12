f67f85a2170373da21a3bc056087c96aca75829d
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:32:03 +0500

    Eleventh string manipulation assignment: not bad

---
+++ Assignment-11-not-bad/Not_Bad.VB/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str As String
+        Dim x As Integer
+        Dim y As Integer
+
+        str = " "
+        x = 0
+        y = 0
+
+
+        Console.Write("String: ")
+        str = Console.ReadLine()
+
+        x = InStr(str, "not")
+        y = InStr(str, "bad") + 2
+
+        If x > 0 And y > 2 And x < y Then
+
+            str = Left(str, x - 1) & "good" & Mid(str, y + 1, Len(str) - y)
+
+        End If
+
+        Console.WriteLine("Result: " & str)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
