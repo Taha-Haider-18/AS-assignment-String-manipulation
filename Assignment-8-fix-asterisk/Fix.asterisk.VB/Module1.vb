d2fa6d43f40ec1adf53fa727cf10578e84481111
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:26:56 +0500

    Eighth string manipulation assignment: fix asterisk

---
+++ Assignment-8-fix-asterisk/Fix.asterisk.VB/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str As String
+        Dim result As String
+        Dim chr As Char
+        Dim i As Integer
+
+        str = ""
+        result = ""
+        chr = ""
+        i = 0
+
+        Console.Write("String: ")
+        str = Console.ReadLine()
+        chr = Left(str, 1)
+
+        For i = 1 To Len(str)
+            If i > 1 Then
+                If chr = Mid(str, i, 1) Then
+                    result = result & "*"
+                Else
+                    result = result & Mid(str, i, 1)
+                End If
+            Else
+                result = chr
+            End If
+        Next
+
+        Console.WriteLine("Result: " & result)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+
+End Module
---
+++ Assignment-8-fix-asterisk/Fix.asterisk.VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Fix.asterisk.VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Fix.asterisk.VB")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("81382254-d47f-47e6-b9c5-07e993c036f5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
