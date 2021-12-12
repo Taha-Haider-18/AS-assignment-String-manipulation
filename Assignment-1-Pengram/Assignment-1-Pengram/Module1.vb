ea8ffa6ebd8181b72c562916fd89752238b5ce14
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 22:47:54 +0500

    First string manipulation assignment: Pengram detector

---
+++ Assignment-1-Pengram/Assignment-1-Pengram/Module1.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+    Sub Main()
+
+        Dim str As String
+        Dim alph As String
+        Dim i As Integer
+        Dim x As Integer
+        Dim ascii As Integer
+
+        str = ""
+        alph = ""
+        i = 1
+        ascii = 96
+        x = 0
+
+        Console.Write("String: ")
+        str = Console.ReadLine()
+        str = LCase(str)
+
+        For i = 1 To 26
+            alph = Chr(ascii + i)
+            x = InStr(str, alph)
+            If x = 0 Then
+
+                Exit For
+
+            End If
+        Next
+        If x = 0 Then
+            Console.WriteLine("String is not a pengram")
+        Else
+            Console.WriteLine("String is a pengram.")
+        End If
+
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
