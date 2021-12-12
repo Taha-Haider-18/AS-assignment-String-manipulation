572d125178734ef37c74eec685273d2946ae7b2a
Taha Haider <write.tahahaider18@gmail.com>
2021-12-12 23:21:31 +0500

    Sixth string manipulation assignment: donut counter

---
+++ Assignment-6-donut-counter/Charlie.And.The.Donut.Factory.VB/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim donut As Integer
+
+        donut = 0
+
+        Console.Write("Donuts? ")
+        donut = Console.ReadLine()
+
+        If donut > 9 Then
+            Console.WriteLine("Number of donuts: many")
+        Else
+            Console.WriteLine("Number of donuts:" & Str(donut))
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+    'Console.WriteLine("Number of donuts:" & IIf(donut <= 9, donut, "many"))
+
+
+End Module
